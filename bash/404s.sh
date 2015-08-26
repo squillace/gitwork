15bc0cdaf243b0884d7ea94b43c6f5284ed7ee44
Ralph Squillace <ralph@squillace.com>
2015-08-25 17:45:57 -0700

    pretty much ready for use

--- bash/404s.sh
+++ bash/404s.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# xargs -I {} curl -sL -w "%{http_code} %{url_effective}\\n" "{}" -o /dev/null | grep ^404
-
 url=$@
-linkstotest=$(($# - 1))
+linkstotest=$# # this passes the number of arguments passed **after** the command name, which is always the first argument
+
 if [[ $# -eq 1 ]];
 	then 
 	echo "no links passed."
@@ -14,26 +13,28 @@ fi
 declare -a missing
 count=0
 
-# echo "test: \$0 is $0"
-for ((i=1; i < $#; i++))
+for ((i=1; i<$(($linkstotest + 1)); i++))
 	do 
-		printf "%d of %d: %s\n" $i $linkstotest "${!i}"
-		lastlinecount=${#thisstring}
-
+# debugging
+#		lastlinecount=${#thisstring}
 #		thisstring="${!i}"
 #		echo "working on: $thisstring"
 #		echo "length of this string is ${#thisstring}"
-#		printf "\r"
+
+# stash any 404 results in the missing array
+		printf "\r%d of %d... ($count bad links so far)" "$i" $linkstotest
 		result=$(curl -sL -w "%{http_code} %{url_effective}\\n" "${!i}" -o /dev/null | grep ^404)
 			if [[ "$result" ]]; then
-				printf " -- %s\n" "$result"
-				missing[$count]='$result'
-				#echo "${404s[$i]}"
+				printf " =====  %s\n" "$result"
+				missing[$count]="$result"
 				((count++))
 			fi
-#		echo "last line count is $lastlinecount"
 done
 
+for ((i=0; i<$count; i++))
+	do
+		echo "${missing[$i]}"
+done
 
 
 
