7c212b23b6593997c706008ad6a2be887b5c514a
Ralph Squillace <ralph@squillace.com>
2016-01-28 18:33:36 -0800

    working with asm/arm and tag detection

--- bash/readcsv.sh
+++ bash/readcsv.sh
@@ -7,9 +7,14 @@ function pause(){
 }
 
 function get_tags() {
-    echo $1
-    find "$GITROOT" -name "$1" -type f -exec grep -ol "$FILE" {} +
-    eval "$2='foo bar rab oof'"
+    #echo $1
+    local FILEPATH=$(find "$GITROOT" -name "$1" -type f)
+    #echo "file is $1"
+    #echo "gitroot is $GITROOT"
+    #echo "getting complete path is "
+    #find "$GITROOT" -name "$1" -type f
+    #echo $(grep -Pohr "(?<=tags=\").*" $FILEPATH | sed s/\".*//g)
+    eval "$2='$(grep -Pohr "(?<=tags=\").*" $FILEPATH | sed s/\".*//g)'"
 }
 
 function isAsm(){
@@ -48,6 +53,10 @@ get_tags virtual-machines-linux-tutorial.md return_var
 echo $return_var
 
 let COUNT=0
+let tags=""
+let asm=""
+let arm=""
+
 while IFS=, read Assigned Title URL contentID Author MSTgtPltfrm NewNameSlug Include Windows Linux RedirectTarget
 do
     ((COUNT++))
@@ -56,22 +65,22 @@ do
         continue
     fi
     
-    return_var=''
-    get_tags virtual-machines-linux-tutorial.md return_var
-    
+    get_tags $contentID.md tags
+    isAsm 
     
     echo "Assigned: $Assigned"
     echo "Title: $Title"
     echo "URL: $URL"
     echo "contentID: $contentID"
     echo "Author: $Author"
-    echo "Tags: $(get_tags $contentID.md return_var)"
+    echo "Tags: $tags"
     echo "MSTgtPltfrm: $MSTgtPltfrm"
     echo "NewNameSlug: $NewNameSlug"
     echo "Include: $Include"
     echo "Windows: $Windows"
     echo "Linux: $Linux"
     echo "RedirectTarget: $RedirectTarget"
+    echo "is it ASM: 
     echo ""
     
     
