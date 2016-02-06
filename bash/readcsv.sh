e4f18e547518bcbb70128e58ab3789a283d04440
Ralph Squillace <ralph@squillace.com>
2016-02-05 16:50:17 -0800

    stable looping, detecting more errors, logging files. Now on to formal name-making.

--- bash/readcsv.sh
+++ bash/readcsv.sh
@@ -25,7 +25,7 @@ function get_tags() {
     #echo "getting complete path is "
     #find "$GITROOT" -name "$1" -type f
     #echo $(grep -Pohr "(?<=tags=\").*" $FILEPATH | sed s/\".*//g)
-    eval "$2='$(grep -Pohr "(?<=tags=\").*" $FILEPATH | sed s/\".*//g)'"
+    eval "$2='$(grep -Pohr "(?<=tags=\").*(?=\"/>)" $FILEPATH )'"
 }
 
 # Extracts the topic title directly from the files
@@ -34,36 +34,63 @@ function get_Title(){
 #    echo "File argument is $1"
 #    echo "gitroot is $GITROOT"
 #    echo "getting complete path is $(find "$GITROOT" -name "$1" -type f)"
-    
-    echo "\"$(grep -Pohr -m 1 "#+.*" $FILEPATH | sed "s/# *//g" | sed "s/<.*>//g")\""
+
+    echo "\"$(grep -Pohr -m 1 "(?<=^#).*" $FILEPATH | sed "s/^ *//g" | sed "s/<.*>//g")\""
 }
 
-# Determines whether something is BOTH 
+# Determines whether something is BOTH
 function asm_arm_or_both(){
     infix=""
     if [[ "$1" =~ .*azure-resource-manager.* && "$1" =~ .*azure-resource-manager.* ]]; then
-        echo "-" 
-    else 
+        echo "-"
+    else
         if [[ "$1" =~ .*azure-resource-manager.* ]]; then
-            echo "arm-" 
-        else 
+            echo "arm-"
+        else
             if [[ "$1" =~ .*azure-service-management.* ]]; then
-                echo "asm-" 
+                echo "asm-"
             fi
-        
-        fi    
+
+        fi
+    fi
+}
+
+#        if [[ "$NewNameSlug" =~ .*asm.* ]]; then
+#            echo "BUT is does have asm in $NewNameSlug..."
+#        fi
+
+
+function windows_linux_both(){
+
+    if [[ "$Windows" =~ .*_.* ]]; then
+             echo "FIRST: It's Windows!!!"
+    elif [[ "$contentID" =~ .*windows.* || "$MSTgtPltfrm" =~ .*windows.* || "$NewNameSlug" =~ .*windows.* ]]; then
+            echo "SECOND PASS: It's STILL Windows!!!"
+            pause "second pass...."
+    elif [[ "$Linux" =~ .*_.* ]]; then
+            echo "FIRST: It's Linux!!!"
+    elif [[ "$contentID" =~ .*linux.* || "$MSTgtPltfrm" =~ .*linux.* || "$NewNameSlug" =~ .*linux.* ]]; then
+            echo "SECOND PASS: It's STILL Linux!!!"
+            pause "second pass...."
     fi
 }
 
 # replaces underscores with hyphens
 function norm_hypens(){
-    echo $1 | sed s/_/-/g 
+    echo $1 | sed s/_/-/g
 }
 
 # takes tags, newnameslug, and OS to construct new name
+## 1. remove any "virtual-machines-" strings.
+## 2. remove any -arm- and set "arm" flag
+## 3. remove any -asm- and set "asm" flag
+## 4. remove any -windows- set "windows" flag
+## 5. remove any -linux- and set "linux" flag
+## 6. take the stem and write: "virtual-machines"-windows|linux-arm|asm-remaining slug
+nonVirtualMachinesCount=0
 function build_new_name(){
     local new_name=""
-    
+
     echo "vms-linux-$(asm_arm_or_both $1)$(norm_hypens $2).md" | sed s/--/-/g
 }
 
@@ -84,8 +111,6 @@ function no_tags()
             echo "$(timestamp): Windows: $Windows" >> $1
             echo "$(timestamp): Linux: $Linux" >> $1
             echo "$(timestamp): RedirectTarget: $RedirectTarget" >> $1
-            
-            
 }
 
 echo "Log file is: $LOG."
@@ -97,28 +122,48 @@ while IFS=, read Assigned URL contentID Author MSTgtPltfrm NewNameSlug Include W
 do
     ((COUNT++))
     echo "Reading line: $COUNT"
+    # skip the header in the CSV file
     if [ "$COUNT" -eq 1 ]; then
         echo "$timestamp: Header line read."
         continue
-
     fi
 
+    # if you can't find the file in the repo, log it and continue on.
+    if [ ! -f $(find "$GITROOT" -name "$contentID.md" -type f) ]; then
+        echo "$(timestamp): The File '$(find "$GITROOT" -name "$contentID.md" -type f)' Does Not Exist" >> $LOG
+        continue 
+    fi
+
+
+#    windows_linux_both
 
     get_tags $contentID.md tags
-    
+    echo "$tags" 
+    pause "those were the tags we found...."
+    if [[ "$Include" =~ .*_.* ]]; then
+        echo "It's an include file....so here we pass the variable to the include script using \"source\""
+    elif [[ "$Windows" =~ .*_.* ]]; then
+        echo "It's a windows target, so move into the rename windows process..."
+    elif [[ "$Linux" =~ .*_.* ]]; then
+        echo "It's a linux target, so move into the rename linux process..."
+    else
+        echo "$(timestamp): Can't detect what OS is the intended target for line $COUNT: $contentID
+    fi
+
     if [[ ! "$tags" =~ .*azure-resource-manager.* && ! "$tags" =~ .*azure-service-management.* ]]; then
-        
+
         echo "hey, we don't have either tag here!!!!!!!!!!!!!!!"
         if [[ "$NewNameSlug" =~ .*asm.* ]]; then
-            echo "BUT is does have asm in $NewNameSlug..."
+            echo "BUT is does have asm in $NewNameSlug... i"
         fi
         # log the fact that we can't do anything with this file and move on
-        no_tags $LOG $Assigned $URL $contentID.md $Author MSTgtPltfrm $(norm_hypens $NewNameSlug) $Include $Windows $Linux $RedirectTarget
-        pause "Press ENTER to continue..."   
+        #no_tags $LOG $Assigned $URL $contentID.md $Author MSTgtPltfrm $(norm_hypens $NewNameSlug) $Include $Windows $Linux $RedirectTarget
+        pause "Press ENTER to continue..."
         continue
     fi
-    
-    
+
+
+     #set -x
     echo "Assigned: $Assigned"
     echo "Title: $(get_Title $contentID.md)"
     echo "URL: $URL"
@@ -132,12 +177,14 @@ do
     echo "Linux: $Linux"
     echo "RedirectTarget: $RedirectTarget"
     echo "newname: $(build_new_name $tags $NewNameSlug)"
-    
-    echo ""       
 
-    pause "Press ENTER to continue..."   
-    
+    echo ""
+
+    #pause "Press ENTER to continue..."
+
     #source ~/workspace/gitwork/bash/renamefile.sh $contentID.md $(build_new_name $tags $NewNameSlug)
     #find $(git rev-parse --show-toplevel) -name "*.md-e" -type f -exec rm {} +
+
+    echo "Final non virtual machines count: $nonVirtualMachinesCount"
 done < $1
 
