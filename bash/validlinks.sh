f1717f31a90242f5887ee1e31d7542c36f5c4d53
Ralph Squillace <ralph@squillace.com>
2016-02-25 11:53:24 -0800

    adding internal link validation

---
+++ bash/validlinks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+links=$(grep -oP "(?<=\]\().*?(?=\))" $1)
+
+#echo "$links"
+
+for link in $links
+    do 
+#    echo $link       
+    if [[ "$link" =~ .*.md.* ]]; then
+        echo "it's a local file: $link"
+        find $(git rev-parse --show-toplevel) -name "$link" -type f
+        echo "got it with ls? $(ls $link | wc -l)"
+    fi
+done
+
