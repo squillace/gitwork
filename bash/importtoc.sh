7f765f9941860b7e83dce3649d15f03325764ab8
Ralph Squillace <ralph@squillace.com>
2016-08-17 18:55:56 -0700

    fixed the bad paths; updated the ordering; validating the directories and stubs. pick up from here.

--- bash/importtoc.sh
+++ bash/importtoc.sh
@@ -16,30 +16,53 @@ if [ $# -ne 2 ]; then
 elif [ $(ls $1 | wc -l) -eq 0 ]; then
     echo "Can't find the input file $1."
     exit 1
-elif [[ ! -d $2 ]]; then
-    echo "hey, the directory doesn't exist!!"
+#elif [[ ! -d $2 ]]; then
+#    echo "hey, the directory doesn't exist!!"
 fi
 
+slugs=$(cat ~/workspace/acom/code/acom/Acom.SharedHelpers/ServiceSlugs.cs | grep -oP "(?<=\").*(?=\")")
+
+for slug in $slugs
+do
+ if [[ ! -d $slug ]]; then
+    echo "hey, the directory $slug doesn't exist, but it's a slug.'"
+fi
+
+done
+
+dirs=$(find . -type d -d 1)
+
+for dir in $dirs
+do
+ dir=${dir/#.\/}
+echo "grepping for the diretory \"$dir\""
+
+ #echo $dir
+ echo $(grep -oP "$dir" ~/workspace/acom/code/acom/Acom.SharedHelpers/ServiceSlugs.cs)
+
+done
+
+exit 0
 echo "Import source file: $1"
 echo "Export file target: $2"
 SOURCE_FILE="$1"
 SOURCE_RESX_FILE=""
-TARGET_FILE="$2/TOC.md"
-
-heads=$(cat $SOURCE_FILE | jq -r '. | keys[]')
+TARGET_FILE="TOC.md"
+# set -x
+heads=$(cat $SOURCE_FILE | jq -r '. | keys_unsorted[]')
 # echo "$heads"
 
 for H1 in $heads
     do
     xpath="//data[@name=\""$H1\""]/value/text()"
-    title=$(xmllint --xpath $xpath  ~/workspace/acom/code/acom/ACOM.Resources/Shared/Lefties/virtual-machines-linux.resx)
-	echo "# [$title]" >> $TARGET_FILE
+    title=$(xmllint --xpath $xpath  $2)
+	echo "# $title" >> $TARGET_FILE
     subheads=$(cat $SOURCE_FILE | jq -r ".$H1 | keys[]")
     #echo "$subheads"
     for H2 in $subheads
         do
             xpath="//data[@name=\""$H2\""]/value/text()"
-            subtitle=$(xmllint --xpath $xpath  ~/workspace/acom/code/acom/ACOM.Resources/Shared/Lefties/virtual-machines-linux.resx)
+            subtitle=$(xmllint --xpath $xpath  $2)
             article_string=$(cat $SOURCE_FILE | jq -r ".$H1[\"$H2\"]")
             article_string=${article_string//acom:/https://azure.microsoft.com}
             article_string=${article_string//msdn:/https://msdn.microsoft.com/en-us/library/azure/}
